d191f236b5f6ba3d7e723b4032b0c3438766424e
Adofo Benedict <adofobenedict06@gmail.com>
2025-09-09 12:10:08 +0000

    Fixed homepage

--- public/logout/logout-script.js
+++ public/logout/logout-script.js
@@ -36,7 +36,7 @@ const interval = setInterval(() => {
         clearInterval(interval);
         setTimeout(() => {
             // Redirect to the intro page
-            window.location.href = "/HTML/Byte Hub/public/intro/index.html";
+            window.location.href = "../../index.html";
         }, 900);
     }
 }, 20);
